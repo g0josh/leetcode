362041ec1e273cf99b5c2f76fbe0fdbdfc1d1c14
mig <jobin.jj@gmail.com>
2020-07-11 22:26:59 +1000

    linked lists

--- scripts/linked_list.py
+++ scripts/linked_list.py
@@ -1,21 +1,79 @@
 from typing import List, Union
 
-class Node(object):
+class ListNode(object):
     def __init__(self, value:int):
         self.value:int = value
-        self.next:Union[Node, None] = None
+        self.next:Union[ListNode, None] = None
 
-def createNode(values:List[Union[int, str]]) -> Node:
+def createNode(values:List[Union[int, str]]) -> ListNode:
     if len(values) < 1:
         return None
 
-    head = Node(values[0])
+    head = ListNode(values[0])
     curr = head
     for i in range(1, len(values)):
-        curr.next = Node(values[i])
+        curr.next = ListNode(values[i])
         curr = curr.next
     return head    
+
+def deleteNode(linkedListHead:ListNode, value:int):
+    if linkedListHead.val == value:
+        return linkedListHead.next
+    curr = linkedListHead
+    while curr.next is not None:
+        if curr.next.val == value:
+            if curr.next.next is None:
+                curr.next = None
+                return linkedListHead
+            else:
+                curr.next = curr.next.next
+                return linkedListHead
+        curr = curr.next
+    return linkedListHead
+
+def reverseList(linkedListHead:ListNode):
+    if linkedListHead.next is None:
+        return linkedListHead
+    new = reverseList(linkedListHead.next)
+    linkedListHead.next.next = linkedListHead
+    linkedListHead.next = None
+    return new
+
+
+def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
+    if l1 is None:
+        return l2
+    if l2 is None:
+        return l1
+    head:ListNode = l1 if l1.value < l2.value else l2
+    merged = head
+    curr1 = l1.next if head == l1 else l1
+    curr2 = l2.next if head == l2 else l2
+    def mergeCurr1():
+        nonlocal curr1, merged
+        merged.next = curr1
+        curr1 = curr1.next
+        merged = merged.next
+    def mergeCurr2():
+        nonlocal curr2, merged
+        merged.next = curr2
+        curr2 = curr2.next
+        merged = merged.next
+
+    while curr1 is not None or curr2 is not None:
+        if curr2 is None:
+            mergeCurr1()
+        elif curr1 is None:
+            mergeCurr2()
+        elif curr1.value < curr2.value:
+            mergeCurr1()
+        else:
+            mergeCurr2()
+    return head
 
 
 if __name__ == "__main__":
     ll = createNode([1])
+    ll2 = createNode([2])
+    # rll = reverseList(ll)
+    mll = mergeTwoLists(ll, ll2)
